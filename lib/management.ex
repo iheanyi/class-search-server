def618c41be78fc1f184c787f0b3e957442f48ad
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2015-12-14 22:00:41 -0600

    Add in command for importing terms into the database initially.

--- lib/management.ex
+++ lib/management.ex
@@ -1,8 +1,23 @@
 defmodule Management do
   # Module responsible for data management.
-  
+  alias ClassSearch.Repo
+  alias ClassSearch.Term
   def import_terms do
     terms = Scraper.fetch_terms
+    |> Enum.map(fn term -> 
+      data = %{name: term.name, tag: term.value}
+      changeset = Term.changeset(%Term{}, data)
+      old_term = Repo.get(Term, term.value)
+      if is_nil old_term do
+        case Repo.insert(changeset) do 
+          {:ok, _term} ->
+            IO.puts "Successfully inserted Term (#{data.tag} - #{data.name})"
+          {:error, changeset} ->
+            IO.puts "Error inserting Term (#{data.tag} - #{data.name})"
+        end
+      end
+    end
+    )
     terms
   end
   
